39a97f4faed41e496b504658388fe62894b87146
unknown <AJSO>
2020-03-18 11:02:36 +0400

    update

--- masterApp/masterApp/urls.py
+++ masterApp/masterApp/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myBlog/', include('myBlog.urls')),
+    path('', include('myBlog.urls')),
 ]
 
 if settings.DEBUG:
