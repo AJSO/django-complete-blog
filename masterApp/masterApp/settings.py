bdd8d1645a5f79a7457d3961ba81fffacc095b4a
Andrew Ecurut Angura <andre.ecurut@gmail.com>
2020-04-13 02:01:26 +0400

    settings.py updated

--- masterApp/masterApp/settings.py
+++ masterApp/masterApp/settings.py
@@ -196,4 +196,10 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+
 
